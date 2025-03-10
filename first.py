9d942c3c054245d22365cef501ce0d46896b899d
rajesh1k <125659156+rajesh1k@users.noreply.github.com>
2025-03-10 22:15:27 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,8 @@
-print('hello rahul')
+def hello(n):
+    for i in range(1,n):
+        if i%2==0:
+            print("even number")
+            
+        else:
+            print("odd number")
+print(hello(7))
